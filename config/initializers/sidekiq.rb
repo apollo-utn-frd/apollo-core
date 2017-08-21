960a8048651d6b6ade190edfb294151697eef19c
Matías Mangiantini <matiasm15@hotmail.com>
2017-08-21 01:07:13 -0300

    Activar Sidekiq en producción

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,29 @@
 # frozen_string_literal: true
 
-# Perform Sidekiq jobs immediately in development,
-# so you don't have to run a separate process.
-# You'll also benefit from code reloading.
 if Rails.env.development?
+  # Perform Sidekiq jobs immediately in development,
+  # so you don't have to run a separate process.
+  # You'll also benefit from code reloading.
   require 'sidekiq/testing'
+
   Sidekiq::Testing.inline!
+else
+  Sidekiq.configure_server do |config|
+    pool_size = ENV.fetch('WORKER_COUNT', 10).to_i + 2
+
+    config.redis = {
+      url: ENV['REDIS_URL'],
+      size: pool_size
+    }
+
+    ActiveRecord::Base.configurations['production']['pool'] = pool_size
+    ActiveRecord::Base.establish_connection
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      url: ENV['REDIS_URL'],
+      size: 5
+    }
+  end
 end
