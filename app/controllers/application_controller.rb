880057c393e2e665ff13d57cec98bac83f001389
Matías Mangiantini <matiasm15@hotmail.com>
2018-09-24 21:58:46 -0300

    Fix authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,17 @@ class ApplicationController < ActionController::Base
   include ActionController::RequestForgeryProtection
   include ErrorHandlingConcern
 
+  helper_method :current_user
+
   protect_from_forgery unless: -> { request.format.json? }
 
   before_action :authenticate_user
 
   protected
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def authenticate_user
     return unless Rails.env.test?
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path
   end
