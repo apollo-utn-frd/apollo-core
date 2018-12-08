33977d7eecb76e3335c728cbf1a6c6119c6fe818
Matías Mangiantini <matiasm15@hotmail.com>
2018-12-08 15:48:11 -0300

    Permit create places without title

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(request.env["omniauth.auth"])
+    user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = user.id
     redirect_to home_path
   end
