cc3c3f5671ba6ffeff62b30cf687ae4a724df617
Federico Sanches <federicohernansanches@gmail.com>
2018-12-08 08:13:27 -0800

    Navbar en home

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to home_path
   end
 
   def destroy
